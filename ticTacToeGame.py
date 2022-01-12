d17e7fb86f6ff0f70800be21545b8ec697797517
Star Balls <starblls@gmail.com>
2022-01-12 14:40:18 -0700

    is_x_turn loop fixed

--- ticTacToeGame.py
+++ ticTacToeGame.py
@@ -18,20 +18,20 @@ def is_x_turn(board):
     count_y = 0
     count_blank = 0
 
-    for space in board:
+    for space in range(len(board)):
         # Count the number of X's and O's on the board. 
-        if space == "X":
+        if board[space] == X:
             count_x += 1
-        elif space == "Y":
+        elif board[space] == O:
             count_y += 1
         else:
             count_blank += 1
 
         # Compare X's and O's to determine whose turn it is to go. 
-        if count_x <= count_y and count_blank != 0:
-            return True
-        else:
-            return False
+    if count_x <= count_y and count_blank != 0:
+        return True
+    else:
+         return False
         
 def quit_game(user_input):
     '''Check if the user inputs a "Q" or "q" to end the game.'''
@@ -64,7 +64,21 @@ def mark_square(user_input, symbol, board):
                "Please enter a number for an empty square.")
     return
 
-def play_game():
+def display_board(board):
+    '''Display a Tic-Tac-Toe board on the screen in a user-friendly way.'''
+    # Put display code here.
+    print(" {} | {} | {}".format(board[0], board[1], board[2]))
+    print("---+---+---")
+    print(" {} | {} | {}".format(board[3], board[4], board[5]))
+    print("---+---+---")
+    print(" {} | {} | {}".format(board[6], board[7], board[8]))  
+
+def game_done(board, message=False):
+    for space in range(len(board)):
+        pass 
+
+
+def play_game(board):
     '''Continue to prompt for X and O until the game is ended. The game ends 
     by one player getting three in a row horizontally, vertically, or 
     diagonally, or neither player wins (a tie), or the game is quit.'''
@@ -78,13 +92,20 @@ def play_game():
             symbol = O
 
         user_input = input(symbol + "> ")
-        end = quit_game(user_input)
 
-        user_input = check_user_input(user_input)
+        # Check if the user wants to end the game by enters q.
+        if quit_game(user_input) == True:
+            end = True
+            return 
+
+        # Check the user input for validity. Is it a number 1 through 9?
+        # No letters or invalid numbers.
+        else:
+            user_input = check_user_input(user_input)
+
+        # Put an X or O on the board and save to the game list of moves.
         mark_square(user_input, symbol, board)
-        # print(board)
-
-
+        display_board(board)
 
     
 print()
@@ -99,7 +120,7 @@ print("\t  7 | 8 | 9 ")
 
 # Create a blank board for tic-tac-toe game.
 board = new_board()
-
+play_game(board)
 
 
 
